60dcb6243a4c40b0e8e6bbee25cc2d3a2e11ce8c
WohnungNDS <67795376+WohnungNDS@users.noreply.github.com>
2022-06-09 17:23:59 +0200

    yearslider

--- _includes/assets/js/plugins/jquery.sdgMap.js
+++ _includes/assets/js/plugins/jquery.sdgMap.js
@@ -411,7 +411,7 @@
           .domain(plugin.valueRange)
           .classes(plugin.options.colorRange.length);
         plugin.years = _.uniq(availableYears).sort();
-        plugin.currentYear = plugin.years[0];
+        plugin.currentYear = plugin.years.slice(-1)[0];
 
         // And we can now update the colors.
         plugin.updateColors();
