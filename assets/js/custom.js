739825f9663114ac5aec3a99f2e0d4f31beb23d7
christoph-LSN <94443931+christoph-LSN@users.noreply.github.com>
2022-11-03 10:23:56 +0000

    Deploying to gh-pages from  @ a8d6089bea0444cef9acc6fb4f22c3d41886f0d2 🚀

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -3,7 +3,6 @@
 opensdg.tableConfigAlter(function(config) {
     var overrides = {
         "order": [[ 0, "desc" ]]
-        "buttons": [[ 'print' ]]
       };
     $.extend(true, config, overrides);
 });
