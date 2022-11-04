ae79ef483d118b92035d035f46c0b7cf19efad40
christoph-LSN <94443931+christoph-LSN@users.noreply.github.com>
2022-11-04 08:41:05 +0100

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,5 +1,26 @@
 // Add any custom javascript here.
 
+// printing
+
+function printDiv() 
+{
+
+  var divToPrint=document.getElementById('indicator-main');
+
+  var newWin=window.open('','Print-Window');
+
+  newWin.document.open();
+
+  newWin.document.write('<html><body onload="window.print()">'+divToPrint.innerHTML+'</body></html>');
+
+  newWin.document.close();
+
+  setTimeout(function(){newWin.close();},10);
+
+}
+
+// table sorting desc.
+
 opensdg.tableConfigAlter(function(config) {
     var overrides = {
         "order": [[ 0, "desc" ]],
