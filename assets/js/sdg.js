ac740ff3858ab6ea7ecc35d7e426bc2b96549a46
christoph-LSN <94443931+christoph-LSN@users.noreply.github.com>
2024-10-11 10:50:14 +0000

    Deploying to gh-pages from  @ d828ab1e393e3329ec9a7133e165759f7bc96151 🚀

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -128,7 +128,7 @@ opensdg.autotrack = function(preset, category, action, label) {
     this.proxy = options.proxy;
     this.proxySerieses = options.proxySerieses;
     this.startValues = options.startValues;
-    this.configObsAttributes = [{"field":"COMMENT_OBS","label":""}];
+    this.configObsAttributes = [{"field":"COMMENT_OBS","label":"Hinweis"}];
     this.allObservationAttributes = options.allObservationAttributes;
 
     // Require at least one geoLayer.
@@ -1367,7 +1367,7 @@ function nonFieldColumns() {
       columns.push(tsAttribute.field);
     });
   }
-  var observationAttributes = [{"field":"COMMENT_OBS","label":""}];
+  var observationAttributes = [{"field":"COMMENT_OBS","label":"Hinweis"}];
   if (observationAttributes && observationAttributes.length > 0) {
     observationAttributes.forEach(function(oAttribute) {
       columns.push(oAttribute.field);
@@ -2556,7 +2556,7 @@ function prepareDataForDataset(years, rows, allObservationAttributes) {
     data: [],
     observationAttributes: [],
   };
-  var configObsAttributes = [{"field":"COMMENT_OBS","label":""}];
+  var configObsAttributes = [{"field":"COMMENT_OBS","label":"Hinweis"}];
   if (configObsAttributes && configObsAttributes.length > 0) {
     configObsAttributes = configObsAttributes.map(function(obsAtt) {
       return obsAtt.field;
@@ -2785,7 +2785,7 @@ function inputEdges(edges) {
       return true;
     });
   }
-  var configuredObservationAttributes = [{"field":"COMMENT_OBS","label":""}];
+  var configuredObservationAttributes = [{"field":"COMMENT_OBS","label":"Hinweis"}];
   if (configuredObservationAttributes && configuredObservationAttributes.length > 0) {
     configuredObservationAttributesFlat = configuredObservationAttributes.map(function(att) { return att.field; });
     edgesData = edgesData.filter(function(edge) {
@@ -2829,7 +2829,7 @@ function getAllObservationAttributes(rows) {
   }
   var obsAttributeHash = {},
       footnoteNumber = 0,
-      configObsAttributes = [{"field":"COMMENT_OBS","label":""}];
+      configObsAttributes = [{"field":"COMMENT_OBS","label":"Hinweis"}];
   if (configObsAttributes && configObsAttributes.length > 0) {
     configObsAttributes = configObsAttributes.map(function(obsAtt) {
       return obsAtt.field;
@@ -3439,7 +3439,7 @@ function updateObservationAttributes(obsAttributes) {
  * Gets the text of an observation attribute for display to the end user.
  */
 function getObservationAttributeText(obsAttribute) {
-    var configuredObsAttributes = [{"field":"COMMENT_OBS","label":""}];
+    var configuredObsAttributes = [{"field":"COMMENT_OBS","label":"Hinweis"}];
     var attributeConfig = _.find(configuredObsAttributes, function(configuredObsAttribute) {
         return configuredObsAttribute.field === obsAttribute.field;
     });
